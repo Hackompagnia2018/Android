a38302ed86c8b4f98941d022828f6fc29f2d9b72
federicobenedetti <fede.benedetti9@gmail.com>
2018-11-30 08:31:39 +0100

    NewSale: add more input

--- app/src/main/java/com/federicobenedetti/res/NewSaleFragment.java
+++ app/src/main/java/com/federicobenedetti/res/NewSaleFragment.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
-public class NewSaleFragment extends Fragment {
+public class NewSaleFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     private OnFragmentInteractionListener mListener;
 
     public NewSaleFragment() {
@@ -27,7 +30,18 @@ public class NewSaleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new_sale, container, false);
+        View v =  inflater.inflate(R.layout.fragment_new_sale, container, false);
+        Spinner spinner = v.findViewById(R.id.spinner_new_sale);
+        spinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(v.getContext(),
+                R.array.places, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        return v;
     }
 
     public void onButtonPressed(Uri uri) {
@@ -40,6 +54,16 @@ public class NewSaleFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     public interface OnFragmentInteractionListener {
