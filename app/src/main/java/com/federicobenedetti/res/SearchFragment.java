5b59fa882881daa108ab9ccc40b4962f49fcf289
federicobenedetti <fede.benedetti9@gmail.com>
2018-11-30 08:43:52 +0100

    SearchFragment: add onClick functionalities and an empty Activity for result

--- app/src/main/java/com/federicobenedetti/res/SearchFragment.java
+++ app/src/main/java/com/federicobenedetti/res/SearchFragment.java
@@ -1,17 +1,22 @@
 package com.federicobenedetti.res;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.Spinner;
 
 
 public class SearchFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private final String TAG = " SearchFragment";
     private OnFragmentInteractionListener mListener;
 
 
@@ -50,6 +55,21 @@ public class SearchFragment extends Fragment implements AdapterView.OnItemSelect
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adapter);
+
+        final SearchView sv = v.findViewById(R.id.searchview_fragment);
+
+
+        Button b = v.findViewById(R.id.search_button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence sv_text = sv.getQuery();
+                Log.i(TAG, "SearchView text: " + sv_text);
+                // Intent intent = new Intent(getContext(), SearchResultActivity.class);
+                // startActivity(intent);
+            }
+        });
 
         return v;
 
